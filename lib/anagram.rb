17851eff0836e73c75386304c58cc74a5f0f940f
Steven Rosario <strosariogarcia@gmail.com>
2022-10-11 16:38:02 -0400

    commit tree 5e0335e622c7c7c80d4c2c6763717d224355f89e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    #array of words
+    def match(words)
+        words.select do |word|
+            word.chars.sort == @word.chars.sort
+        end
+    end
+end
